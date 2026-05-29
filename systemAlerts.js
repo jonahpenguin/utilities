c20eab879154a9fa1a3473fe0b77e685ec8a5eae
Jonah DeRenzo <jderenzo07@gmail.com>
2026-05-29 18:13:06 +0000

    Removed alert 5.26.26 and localStorage entry

--- systemAlerts.js
+++ systemAlerts.js
@@ -1,6 +1,7 @@
-if (!localStorage.getItem("alert5.26.26")) {
-    let isSure = confirm("Heads up! This website will be down for maintenance May 26th at 9pm (local time)\nPress OK to not show this again")
-    if (isSure) {
-        localStorage.setItem('alert5.26.26',true);
-    }
+if (localStorage.getItem("alert5.26.26")) {
+    // let isSure = confirm("Heads up! This website will be down for maintenance May 26th at 9pm (local time)\nPress OK to not show this again")
+    // if (isSure) {
+        // localStorage.setItem('alert5.26.26',true);
+    // }
+    localStorage.removeItem('alert5.26.26');
 }
