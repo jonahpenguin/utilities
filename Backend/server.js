02f7fb2d46c2d37d3bd1b270318917bb1f178874
jonahpenguin <jderenzo07@gmail.com>
2026-07-14 14:32:15 +0000

    Add Backend

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 3000);
