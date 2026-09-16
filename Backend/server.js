e6c143b43eeb5a77ca721fb33260c57289bc32af
jonahpenguin <jderenzo07@gmail.com>
2026-09-15 20:51:04 -0400

    Remove username connection check in HSroomCheck
    Removed check for existing username in game room.

--- Backend/server.js
+++ Backend/server.js
@@ -169,10 +169,6 @@ io.on("connection", (socket) => {
     let username = msg.split(",")[0];
     let roomID = parseInt(msg.split(",")[1]);
     let gameID = hsGames.map(function (e) {return e.roomID}).indexOf(roomID);
-    if (hsGames[gameID].players.map(function (e) {return e.username}).includes(username)) {
-      socket.emit("HSroomFail", "Already connected");
-      return;
-    }
     if (gameID != -1) {
       if (hsGames[gameID].isLocked) {
         socket.emit("HSroomFail", "Room is locked");
